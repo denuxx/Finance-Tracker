6c05becfe42bc31c3ecab605a45e9a3c736e0839
denux <nik451523@gmail.com>
2024-05-03 16:48:30 +0300

    Initial Commit

---
+++ Finance-Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Finance-Tracker
+//
+//  Created by Никита on 03.05.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Finance-Tracker/Finance_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Finance_TrackerApp.swift
+//  Finance-Tracker
+//
+//  Created by Никита on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct Finance_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
