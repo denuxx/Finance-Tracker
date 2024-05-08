b789a348d64ef77727b726084d01562b3a69d4a2
denux <nik451523@gmail.com>
2024-05-08 20:00:17 +0300

    Сделал интерфейс для редактирования транзакций, Насройки, view для выбора даты рассчета. Настроил SwiftData доступны функции CRUD

---
+++ Finance-Tracker/View/Helpers/DateFilterView.swift
@@ -0,0 +1,18 @@
+//
+//  DateFilterView.swift
+//  Finance-Tracker
+//
+//  Created by Никита on 08.05.2024.
+//
+
+import SwiftUI
+
+struct DateFilterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateFilterView()
+}
---
+++ Finance-Tracker/View/Helpers/TransactionCardView.swift
@@ -0,0 +1,18 @@
+//
+//  TransactionCardView.swift
+//  Finance-Tracker
+//
+//  Created by Никита on 04.05.2024.
+//
+
+import SwiftUI
+
+struct TransactionCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TransactionCardView()
+}
